6792c1b5d5ea8a43cf56b13265304425ae5735b4
Brenton Forwood <forwoodb77@gmail.com>
2026-07-19 23:27:58 -0400

    Reset yfinance data index and added 'ticker' column

--- stock-dashboard-2/yahoo.py
+++ stock-dashboard-2/yahoo.py
@@ -12,4 +12,12 @@ collection = db['stocks']
 # Get stock data from yfinance
 for stock in collection.find():
   data = yf.download(tickers=stock['ticker'], period='200d', interval='1d')
+
+  # Reset the index to make 'Date' a regular column
+  data.reset_index(inplace=True)
+
+  # df = pd.DataFrame(data)
+
+  # data = data.rename(columns={'Ticker': 'ticker'})
+  data['ticker'] = stock['ticker']
   print(data)
